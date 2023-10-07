8cc0e448e5fa3875449745aa020c9db253658539
larroquejl@gmail.com <larroquejl@gmail.com>
2023-10-07 12:46:13 -0400

    Thought model completed

--- models/Thought.js
+++ models/Thought.js
@@ -11,6 +11,34 @@ const thoughtSchema = new Schema(
         username: {
             type: String,
             required: [true, 'Please provide a username']
-        }
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+            get: (createdAt) => 
+            formatDate(
+                Intl.DateTimeFormate('en-US', {
+                    month: 'short',
+                    day: 'numeric',
+                    year: 'numeric',
+                    hour: 'numeric',
+                    minute: 'numeric',
+                }).format(new Date(createdAt))
+            ),
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
     }
-)
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+module.exports = model('thought', thoughtSchema);
