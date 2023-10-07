2d5517abc02e95e211e30e00ad2ce02bf72dd0c3
larroquejl@gmail.com <larroquejl@gmail.com>
2023-10-07 11:46:59 -0400

    User model added.

---
+++ models/User.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref:'thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'user',
+            },
+        ],
+        username: {
+            type: String,
+            required: [true, 'Please provide a username'],
+            unique: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: [true, 'Please provide email'],
+            match: [
+                /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+        'Please provide valid email'
+            ],
+            unique: true,
+        },
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+module.exports = model('user', userSchema);
